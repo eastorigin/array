fda64d94896d012ebe85a886a5cceb51d91f86b8
Shin Dong Won <eastorigin21@gmail.com>
2024-07-23 17:49:34 +0900

    숙제 중

---
+++ src/com/ktdsuniversity/edu/array/Goods.java
@@ -0,0 +1,14 @@
+package com.ktdsuniversity.edu.array;
+
+public class Goods {
+
+	private String goodsName;
+	private int goodsPrice;
+	private int goodsStock;
+	
+	public Goods(String goodsName, int goodsPrice, int goodsStock) {
+		this.goodsName = goodsName;
+		this.goodsPrice = goodsPrice;
+		this.goodsStock = goodsStock;
+	}
+}
---
+++ src/com/ktdsuniversity/edu/array/Seller.java
@@ -0,0 +1,10 @@
+package com.ktdsuniversity.edu.array;
+
+public class Seller {
+
+	private Goods[] goods;
+	
+	public void sell(String goodsName, int orderNumber ) {
+		
+	}
+}
